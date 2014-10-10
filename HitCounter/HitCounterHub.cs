c16cd5444f6d6055df48b69931204d918f80a0f8
Matthew Padden <paddenmatthew@live.ie>
2014-10-10 16:19:50 +0100

    First commit

---
+++ HitCounter/HitCounterHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace HitCounter
+{
+    [HubName("hitCounter")]
+    public class HitCounterHub : Hub
+    {
+        static int _hitCount = 0;
+
+        public void RecordHit()
+        {
+            _hitCount++;
+            Clients.All.onRecordHit(_hitCount);
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            _hitCount--;
+            Clients.All.onRecordHit(_hitCount);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
